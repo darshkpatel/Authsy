1a927499c56f35e4cf1fc49e9ba0ddd377fb84fd
umangraval <umangraval2000@gmail.com>
2021-02-16 10:34:42 +0530

    local jwt done, oauth cb err left

--- authsy_backend/src/models/user.model.js
+++ authsy_backend/src/models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const { toJSON, paginate } = require('./plugins');
 // const { roles } = require('../config/roles');
 
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema(
   {
     googleId: {
       type: String,
-      required: true,
+      // required: true,
       trim: true,
     },
     name: {
@@ -32,19 +32,20 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
       trim: true,
+      default: 'https://res.cloudinary.com/brij1999/image/upload/v1574787385/Connect_plus/user_purple.png',
     },
-    // password: {
-    //   type: String,
-    //   required: true,
-    //   trim: true,
-    //   minlength: 8,
-    //   validate(value) {
-    //     if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-    //       throw new Error('Password must contain at least one letter and one number');
-    //     }
-    //   },
-    //   private: true, // used by the toJSON plugin
-    // },
+    password: {
+      type: String,
+      // required: true,
+      trim: true,
+      // minlength: 8,
+      // validate(value) {
+      //   if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+      //     throw new Error('Password must contain at least one letter and one number');
+      //   }
+      // },
+      private: true, // used by the toJSON plugin
+    },
     // role: {
     //   type: String,
     //   enum: roles,
@@ -71,23 +72,23 @@ userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
   return !!user;
 };
 
-// /**
-//  * Check if password matches the user's password
-//  * @param {string} password
-//  * @returns {Promise<boolean>}
-//  */
-// userSchema.methods.isPasswordMatch = async function (password) {
-//   const user = this;
-//   return bcrypt.compare(password, user.password);
-// };
+/**
+ * Check if password matches the user's password
+ * @param {string} password
+ * @returns {Promise<boolean>}
+ */
+userSchema.methods.isPasswordMatch = async function (password) {
+  const user = this;
+  return bcrypt.compare(password, user.password);
+};
 
-// userSchema.pre('save', async function (next) {
-//   const user = this;
-//   if (user.isModified('password')) {
-//     user.password = await bcrypt.hash(user.password, 8);
-//   }
-//   next();
-// });
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 
 /**
  * @typedef User
