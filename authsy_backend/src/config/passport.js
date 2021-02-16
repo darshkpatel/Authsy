aba259d07e9930bef832b787bd11fd6f48c02200
umangraval <umangraval2000@gmail.com>
2021-02-16 11:22:30 +0530

    cb err fix, cleanup

--- authsy_backend/src/config/passport.js
+++ authsy_backend/src/config/passport.js
@@ -5,7 +5,6 @@ const config = require('./config');
 const { getUserBygoogleId } = require('../services/user.service');
 const { tokenTypes } = require('./tokens');
 const { User } = require('../models');
-// const catchAsync = require('../utils/catchAsync');
 const { userService, tokenService } = require('../services');
 
 const jwtOptions = {
@@ -30,35 +29,18 @@ const jwtVerify = async (payload, done) => {
 
 const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
 
-// function generateUserToken(req, res) {
-//   const accessToken = token.generateAccessToken(req.user.id);
-//   res.send(accessToken);
-// }
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser((id, done) => {
-//   getUserBygoogleId(id).then((user) => {
-//     done(null, user);
-//   });
-// });
-
 passport.use(
   new GoogleStrategy(
     {
       clientID: config.auth.googleId,
       clientSecret: config.auth.googleSecret,
       callbackURL: config.auth.callbackURL,
-      // passReqToCallback: true,
     },
     async (accessToken, refreshToken, profile, done) => {
-      // console.log(profile);
       const user = await getUserBygoogleId(profile.id);
       if (user) {
         const tokens = await tokenService.generateAuthTokens(user);
-        done(null, { user, tokens });
+        return done(null, { user, tokens });
       }
       const newuser = await userService.createUser({
         googleId: profile.id,
@@ -67,16 +49,7 @@ passport.use(
         photo: profile.photos[0].value.split('?')[0],
       });
       const tokens = await tokenService.generateAuthTokens(newuser);
-      done(null, { newuser, tokens });
-      // res.status(httpStatus.CREATED).send({ user, tokens });
-      //     createUser({
-      //       googleId: profile.id,
-      //       name: profile.displayName,
-      //       token: accessToken,
-      //       email: profile.emails[0].value,
-      //       photo: profile.photos[0].value.split('?')[0],
-      //     }).then((user) => done(null, user));
-      //   }
+      return done(null, { newuser, tokens });
     }
   )
 );
--- authsy_web/src/App.jsx
+++ authsy_web/src/App.jsx
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const Login = lazy(() => import("./views/Login"));
 const Landing = lazy(() => import("./views/Landing"));
+const Dashboard = lazy(() => import("./views/Dashboard"));
 
 const App = () => (
   <Router>
@@ -10,9 +11,13 @@ const App = () => (
         <Route path="/login">
           <Login />
         </Route>
+        <Route path="/dash">
+          <Dashboard />
+        </Route>
         <Route path="/">
           <Landing />
         </Route>
+        
       </Switch>
     </Suspense>
   </Router>
