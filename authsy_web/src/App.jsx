e8d0adec904eb42296d96ec7c2328bc57b5e9d2c
Darsh <darshkpatel@gmail.com>
2021-02-17 11:16:27 +0530

    switch to react-use for device mediaquery

--- authsy_web/src/App.jsx
+++ authsy_web/src/App.jsx
@@ -1,9 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import {
-  BrowserView,
-  MobileView,
-} from "react-device-detect";
+import { useMedia } from 'react-use';
 const Login = lazy(() => import("./views/Login"));
 const Landing = lazy(() => import("./views/Landing"));
 const Dashboard = lazy(() => import("./views/Dashboard"));
@@ -11,30 +8,36 @@ const Signup = lazy(() => import("./views/Signup"));
 const MobileDashboard = lazy(() => import("./views/mobile/Dashboard"));
 const AuthSucess = lazy(() => import("./views/AuthSucess"));
 
-const App = () => (
-  <Router>
-    <BrowserView>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/dash" component={Dashboard} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/auth/success" component={AuthSucess} />
-        <Route path="/" component={Landing} />
-      </Switch>
-    </Suspense>
-    </BrowserView>
-
-    <MobileView>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Switch>
-        <Route path="/mobile" component={MobileDashboard} />
-        <Redirect from="/" to="/mobile"/>     
-      </Switch>
-    </Suspense>
-    </MobileView>
-
-  </Router>
-);
+const App = () => {
+  const isDesktop = useMedia('(min-width: 900px)');
+  if (isDesktop) {
+    return (
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/dash" component={Dashboard} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/auth/success" component={AuthSucess} />
+            <Route path="/" component={Landing} />
+            <Redirect from="/mobile" to="/" /> 
+          </Switch>
+        </Suspense>
+      </Router>
+    );
+  }
+  else {
+    return (
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/mobile" component={MobileDashboard} />
+            <Redirect from="/" to="/mobile" />
+          </Switch>
+        </Suspense>
+      </Router>
+    )
+  }
+}
 
 export default App;
