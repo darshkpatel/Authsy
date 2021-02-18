37e236014e31e64eb7334a786ee01342150ee8af
Darsh <darshkpatel@gmail.com>
2021-02-18 11:35:03 +0530

    cleanup redudant classes

--- authsy_web/src/components/Navbar.js
+++ authsy_web/src/components/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar(props) {
         className={
           (props.transparent
             ? "top-0 absolute z-50 w-full"
-            : "relative shadow-lg bg-white shadow-lg") +
+            : "relative shadow-lg bg-white") +
           " flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
         }
       >
