a5e6f9ba70f56e253b996d0ac996b151e25d45d6
umangraval <umangraval2000@gmail.com>
2021-02-19 19:57:11 +0530

    quietjs transmit sample sound from mobile

--- authsy_web/src/views/mobile/Dashboard.jsx
+++ authsy_web/src/views/mobile/Dashboard.jsx
@@ -4,6 +4,17 @@ import Navbar from "../../components/Navbar.js";
 import FooterSmall from "../../components/FooterSmall.js";
 
 export default function Login() {
+  window.Quiet.init({
+    profilesPrefix: "/",
+    memoryInitializerPrefix: "/",
+    libfecPrefix: "/"
+  });
+  function sendOTP(e) {
+    var payload = "hello";
+    var profilename = "audible";
+    var transmit = window.Quiet.transmitter({ profile: profilename });
+    transmit.transmit(window.Quiet.str2ab(payload));
+  }
   return (
     <>
       <Navbar transparent />
@@ -23,7 +34,18 @@ export default function Login() {
               <div className="w-full lg:w-4/12 px-4">
                 <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
                   <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                  <h1>MOBILE VIEW</h1>
+                    <h1>MOBILE VIEW</h1>
+                    <h2>payload = hello</h2>
+                    <button
+                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                      type="button"
+                      style={{ transition: "all .15s ease" }}
+                      onClick={() => {
+                        sendOTP();
+                      }}
+                    >
+                      Send OTP
+                    </button>
                   </div>
                 </div>
               </div>
