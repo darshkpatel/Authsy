81de6c3b7b192c7f28d1321c1cc8f7873bd5814b
Darsh <darshkpatel@gmail.com>
2021-02-23 22:56:05 +0530

    Proper Redirect for signup

--- authsy_web/src/views/AuthSucess.jsx
+++ authsy_web/src/views/AuthSucess.jsx
@@ -3,6 +3,7 @@ import { getCookie } from 'react-use-cookie';
 import {Redirect } from "react-router-dom";
 export default function Login() {
     let cookie = null;
+    let alreadySignedUp = localStorage.getItem('signed_up') || false;
     try{
    cookie = JSON.parse(getCookie('JWT'));
     }
@@ -12,11 +13,13 @@ export default function Login() {
   if(cookie) {
     localStorage.setItem('access_token', cookie.accessToken)
     localStorage.setItem('refresh_token', cookie.refreshToken)
+    localStorage.setItem('signed_up', true)
   }
   return(
       <>
       {!cookie && <div>Error Authenticating</div>}
-      {cookie && <div>Redirecting..<Redirect to={'/dash'}/></div>}
+      {cookie && !alreadySignedUp && <div>Redirecting..<Redirect to={'/addDevice'}/></div>}
+      {cookie && alreadySignedUp && <div>Redirecting..<Redirect to={'/dash'}/></div>}
       </>
   )
 }
