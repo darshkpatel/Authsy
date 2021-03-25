0d9260235955ba4d9ffdb2429759d3419cc52f01
umangraval <umangraval2000@gmail.com>
2021-03-25 10:27:06 +0530

    auth perm changed to 2FA

--- authsy_backend/src/routes/v1/knock.route.js
+++ authsy_backend/src/routes/v1/knock.route.js
@@ -8,12 +8,12 @@ const router = express.Router();
 
 router
   .route('/:userId')
-  .post(auth('getUsers'), validate(knockValidation.addIP), knockController.createIP)
-  .get(auth('getUsers'), validate(knockValidation.getIPs), knockController.getIPs);
+  .post(auth('2FA'), validate(knockValidation.addIP), knockController.createIP)
+  .get(auth('2FA'), validate(knockValidation.getIPs), knockController.getIPs);
 
 router
   .route('/ip/:ipId')
-  .delete(auth('getUsers'), validate(knockValidation.deleteIP), knockController.deleteIP)
-  .post(auth('getUsers'), validate(knockValidation.knockPort), knockController.portknock);
+  .delete(auth('2FA'), validate(knockValidation.deleteIP), knockController.deleteIP)
+  .post(auth('2FA'), validate(knockValidation.knockPort), knockController.portknock);
 
 module.exports = router;
