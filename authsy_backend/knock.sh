161fea48ea49ca7193234653fcadd09c79fba684
umangraval <umangraval2000@gmail.com>
2021-03-25 08:45:14 +0530

    check dir path

--- authsy_backend/knock.sh
+++ authsy_backend/knock.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-cd ~/go/src/knockandgo
+if [ -d "${HOME}/go/src/github.com/giuliocomi/knockandgo" ] 
+then
+    cd ~/go/src/github.com/giuliocomi/knockandgo
+elif [ -d "${HOME}/go/src/knockandgo" ]
+then
+    cd ~/go/src/knockandgo
+else
+    echo "Error: Directory does not exists."
+fi
 go run main.go -m c -a $1 -s $2 -i $3 -t 300 -k $4
