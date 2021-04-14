620a362ef31818218b34fd7f786e2e6ea476a7f3
Darsh <darshkpatel@gmail.com>
2021-04-14 12:38:37 +0530

    switch to query params

--- authsy_backend/src/controllers/auth.controller.js
+++ authsy_backend/src/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 const httpStatus = require('http-status');
+const base32 = require('thirty-two');
+const crypto = require('crypto');
 const catchAsync = require('../utils/catchAsync');
 const { authService, userService, tokenService, emailService } = require('../services');
-const base32 = require('thirty-two');
-var crypto = require('crypto');
 const { totp } = require('../lib/totp');
+
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
   const tokens = await tokenService.generateAuthTokens(user);
@@ -41,103 +42,91 @@ const resetPassword = catchAsync(async (req, res) => {
 const googleCallback = catchAsync(async (req, res) => {
   const accessToken = req.user.tokens.access.token;
   const refreshToken = req.user.tokens.refresh.token;
-  res.cookie('JWT', JSON.stringify({ accessToken, refreshToken }), {
-    maxAge: 1000 * 30 * 60,
-    httpOnly: false,
-    sameSite: true,
-  })
+
   if (process.env.NODE_ENV === 'production')
-    res.redirect(`${process.env.BASE_URL}/auth/success`);
-  else res.redirect(`${process.env.DEV_BASE_URL}/auth/success`);
-})
+    res.redirect(`${process.env.BASE_URL}/auth/success?accessToken=${accessToken}&refreshToken=${refreshToken}`);
+  else res.redirect(`${process.env.DEV_BASE_URL}/auth/success?accessToken=${accessToken}&refreshToken=${refreshToken}`);
+});
 
 const totpSecretGenerate = catchAsync(async (req, res) => {
-  var secret = base32.encode(crypto.randomBytes(16));
-  //Discard equal signs (part of base32, 
-  //not required by Google Authenticator)
-  //Base32 encoding is required by Google Authenticator. 
-  //Other applications
-  //may place other restrictions on the shared key format.
-  const user = req.user
+  let secret = base32.encode(crypto.randomBytes(16));
+  // Discard equal signs (part of base32,
+  // not required by Google Authenticator)
+  // Base32 encoding is required by Google Authenticator.
+  // Other applications
+  // may place other restrictions on the shared key format.
+  const { user } = req;
   if (!req.user.key) {
     secret = secret.toString().replace(/=/g, '');
-    const updated = await userService.updateUserKeyById(user._id, secret)
-    res.send({ user: updated })
+    const updated = await userService.updateUserKeyById(user._id, secret);
+    res.send({ user: updated });
   } else {
-    res.send({ message: 'Key already exists' })
+    res.send({ message: 'Key already exists' });
   }
-})
+});
 
 const totpVerify = catchAsync(async (req, res) => {
   if (!req.user.key) {
-    res.send({ message: 'Error! No Key Configured' })
-  }
-  else {
-    const result = totp.verify(req.body.totp, req.user.key)
-    if(result && result.delta == 0){
-      res.send({ message: 'Verified'})
-    }
-    else{
-      res.send({ message: 'Invalid'})
+    res.send({ message: 'Error! No Key Configured' });
+  } else {
+    const result = totp.verify(req.body.totp, req.user.key);
+    if (result && result.delta === 0) {
+      res.send({ message: 'Verified' });
+    } else {
+      res.send({ message: 'Invalid' });
     }
   }
-
-})
+});
 
 const totpTokenGen = catchAsync(async (req, res) => {
   if (!req.user.key) {
-    res.send({ message: 'Error! No Key Configured' })
-  }
-  else {
-    const result = totp.verify(req.body.totp, req.user.key)
-    if(result && result.delta == 0){
-      const tokens = await tokenService.generate2FATokens(req.user)
-      res.send({ message: 'Verified', tokens})
-    }
-    else{
-      res.send({ message: 'Invalid'})
+    res.send({ message: 'Error! No Key Configured' });
+  } else {
+    const result = totp.verify(req.body.totp, req.user.key);
+    if (result && result.delta === 0) {
+      const tokens = await tokenService.generate2FATokens(req.user);
+      res.send({ message: 'Verified', tokens });
+    } else {
+      res.send({ message: 'Invalid' });
     }
   }
-
-})
+});
 
 const totpSecretQR = catchAsync(async (req, res) => {
-  var url = null;
-  const user = await userService.getUserById(req.user._id)
+  let url = null;
+  const user = await userService.getUserById(req.user._id);
   if (user.key) {
-    var qrData = `otpauth://totp/Authsy:${user.email}?secret=${user.key}`
-    url = "https://chart.googleapis.com/chart?chs=166x166&cht=qr&chl=" +
-      qrData;
+    const qrData = `otpauth://totp/Authsy:${user.email}?secret=${user.key}`;
+    url = `https://chart.googleapis.com/chart?chs=166x166&cht=qr&chl=${qrData}`;
   }
   res.send({
     user: req.user,
-    qrUrl: url
+    qrUrl: url,
   });
-})
+});
 
 const setMobileConfigured = catchAsync(async (req, res) => {
-  const user = req.user
+  const { user } = req;
   if (!req.user.key) {
-    res.send({ message: 'Error! No Key Configured' })
+    res.send({ message: 'Error! No Key Configured' });
   } else {
-    //ToDo: Check if deleting the key at this point is viable
-    const updated = await userService.updateUserMobileConfig(user._id, true)
-    res.send({ user: updated })
+    // ToDo: Check if deleting the key at this point is viable
+    const updated = await userService.updateUserMobileConfig(user._id, true);
+    res.send({ user: updated });
   }
-})
+});
 
 const getKey = catchAsync(async (req, res) => {
   if (!req.user.key) {
-    res.send({ message: 'Error! No Key Configured' })
+    res.send({ message: 'Error! No Key Configured' });
   } else if (req.user.mobileConfigured) {
-    res.send({ message: 'Error! Key already Configured' })
+    res.send({ message: 'Error! Key already Configured' });
+  } else {
+    // ToDo: Check if deleting the key at this point is viable
+    const key = String(req.user.key);
+    res.send({ key });
   }
-  else {
-    //ToDo: Check if deleting the key at this point is viable
-    const key = String(req.user.key)
-    res.send({ key })
-  }
-})
+});
 
 module.exports = {
   register,
@@ -152,5 +141,5 @@ module.exports = {
   setMobileConfigured,
   getKey,
   totpVerify,
-  totpTokenGen
+  totpTokenGen,
 };
