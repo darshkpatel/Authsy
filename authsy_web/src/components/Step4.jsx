d50e3c30f0d4250a5a863bd5d78ffa3300babab5
Darsh <darshkpatel@gmail.com>
2021-04-14 13:00:38 +0530

    Fix: Dashboard URL

--- authsy_web/src/components/Step4.jsx
+++ authsy_web/src/components/Step4.jsx
@@ -16,7 +16,7 @@ export default function Step4() {
                             </div>
                             <img src={SuccessGif} alt=".." style={{ width: "100%" }} />
                             <div className="text-center mb-3">
-                                <Link to="/dashboard">
+                                <Link to="/dash">
                                     <button
                                         className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
                                         type="button"
